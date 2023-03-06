e8c7d6e43ffccafaea44ca9341bd81dc62bccfa2
李子睿 <lizr5@spdb.com.cn>
2023-03-06 23:52:14 +0800

    [20230306]内存检测

--- src/main/java/io/netty/learn/server/codec/handler/OrderServerProcessHandler.java
+++ src/main/java/io/netty/learn/server/codec/handler/OrderServerProcessHandler.java
@@ -9,12 +9,15 @@ import io.netty.learn.common.Operation;
 import io.netty.learn.common.OperationResult;
 import io.netty.learn.common.RequestMessage;
 import io.netty.learn.common.ResponseMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
 
 //为什么继承SimpleChannelInboundHandler而不是继承ChannelInboundHandler？  Simple帮我们自动释放Bytebuf
 public class OrderServerProcessHandler extends SimpleChannelInboundHandler<RequestMessage> {
+    private static final Logger log = LoggerFactory.getLogger(OrderServerProcessHandler.class);
     @Override
     public void channelRead0(ChannelHandlerContext ctx, RequestMessage requestMessage){
 
@@ -28,6 +31,12 @@ public class OrderServerProcessHandler extends SimpleChannelInboundHandler<Reque
         Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
 
         //加急式，每次写都flush
-        ctx.writeAndFlush(responseMessage);
+        //该if用于防止内存泄露,若没有else,则属于挖坑操作。
+        if(ctx.channel().isActive()&&ctx.channel().isWritable()){
+            ctx.writeAndFlush(responseMessage);
+        }else{
+            log.error("msg dropped");
+        }
+
     }
 }
