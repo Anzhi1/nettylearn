9e89dd58f559794caeec0426a4981370526767cc
Anzhi <until.forever@live.cn>
2023-04-12 14:14:52 +0800

    [20230412]增加客户端重连机制

--- src/main/java/io/netty/learn/client/ClientV1.java
+++ src/main/java/io/netty/learn/client/ClientV1.java
@@ -5,6 +5,7 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
@@ -16,38 +17,80 @@ import io.netty.learn.client.codec.dispatcher.ClientIdleCheckHandler;
 import io.netty.learn.client.codec.dispatcher.KeepAliveHandler;
 import io.netty.learn.common.RequestMessage;
 import io.netty.learn.util.IdUtil;
+import io.netty.util.concurrent.DefaultThreadFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ClientV1 {
+
+    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
+    private static EventLoopGroup eventLoopGroup = new NioEventLoopGroup(0,new DefaultThreadFactory("client"));
+
+    public static final String LOCAL_IP = "127.0.0.1";
+
+    public static final Integer PORT = 8090;
+
+    private static final Logger log = LoggerFactory.getLogger(ClientV1.class);
+
+
     //客户端不区分Handler与childHandler，只有普通的Handler
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Bootstrap bootstrap = new Bootstrap();
-        bootstrap.channel(NioSocketChannel.class)
-                .group(new NioEventLoopGroup())
-                .handler(new ChannelInitializer<NioSocketChannel>() {
-                    @Override
-                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
-                        ChannelPipeline pipeline = nioSocketChannel.pipeline();
-                        pipeline.addLast(new ClientIdleCheckHandler())
-                                .addLast(new OrderFrameDecoder())
-                                .addLast(new OrderFrameEncoder())
-                                .addLast(new OrderProtocolEncoder())
-                                .addLast(new OrderProtocolDecoder())
-                                .addLast("keepAlive",new KeepAliveHandler())
-                                .addLast(new OperationToRequestMessageEncoder())
-                                .addLast(new LoggingHandler(LogLevel.INFO));
+    public void connect(String ip,int port) throws ExecutionException, InterruptedException {
+        try{
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class)
+                    .group(eventLoopGroup)
+                    .handler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                            ChannelPipeline pipeline = nioSocketChannel.pipeline();
+                            pipeline.addLast(new ClientIdleCheckHandler())
+                                    .addLast(new OrderFrameDecoder())
+                                    .addLast(new OrderFrameEncoder())
+                                    .addLast(new OrderProtocolEncoder())
+                                    .addLast(new OrderProtocolDecoder())
+                                    .addLast("keepAlive",new KeepAliveHandler())
+                                    .addLast(new OperationToRequestMessageEncoder())
+                                    .addLast(new LoggingHandler(LogLevel.INFO));
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect(ip, port);
+
+            channelFuture.sync();
+            AuthOperation authOperation = new AuthOperation("admin1","122");
+            OrderOperation orderOperation = new OrderOperation(1001, "tudou");
+            //future是异步执行的
+            channelFuture.channel().writeAndFlush(authOperation);
+            channelFuture.channel().writeAndFlush(orderOperation);
+            channelFuture.channel().closeFuture().get();
+        }
+        finally {
+            //所有资源释放完成之后，清空资源，再次发起重连操作
+            log.info("触发重连");
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                        try {
+                            connect(LOCAL_IP,PORT);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }catch (InterruptedException e1){
+                        e1.printStackTrace();
                     }
-                });
-        ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 8090);
+                }
+            });
+        }
+    }
 
-        channelFuture.sync();
-        AuthOperation authOperation = new AuthOperation("admin1","122");
-        OrderOperation orderOperation = new OrderOperation(1001, "tudou");
-        //future是异步执行的
-        channelFuture.channel().writeAndFlush(authOperation);
-        channelFuture.channel().writeAndFlush(orderOperation);
-
-        channelFuture.channel().closeFuture().get();
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new ClientV1().connect(LOCAL_IP,PORT);
     }
 }
